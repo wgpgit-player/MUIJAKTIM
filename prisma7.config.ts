9dda423fef547be6db6e8b422425854ab66a9c82
mac <mac@Naisss-2.local>
2026-09-11 22:57:20 +0700

    Setup Prisma schema dengan Supabase

---
+++ prisma7.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DIRECT_URL"],  // sementara pakai DIRECT_URL untuk introspeksi
+  },
+});
